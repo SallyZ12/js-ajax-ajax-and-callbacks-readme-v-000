6af2eafdeb79a736f509e152bc154b1819058de2
github username <github email address>
2018-12-08 16:11:21 -0500

    minor spacing changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
     // Now we start the Ajax GET request. The first parameter is the URL with the data.
     // The second parameter is a function that handles the response.
+    
     $.get('sentence.html', function(response) {
         // Here we are getting the element on the page with the id of sentences and
         // inserting the response
